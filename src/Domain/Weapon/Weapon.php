b7ac8a9744f1740e9268445f77fbea96635a7b53
Ismael Moral <jastertdc@gmail.com>
2022-11-19 10:28:27 +0100

    feat(attribute-fuse): added weapon fuse, and check if weapons to fuse are from the same weapon type

--- src/Domain/Weapon/Weapon.php
+++ src/Domain/Weapon/Weapon.php
@@ -7,6 +7,7 @@ namespace JasterTDC\Warriors\Domain\Weapon;
 use JasterTDC\Warriors\Domain\Shared\Name\FullName;
 use JasterTDC\Warriors\Domain\Weapon\Attribute\Attribute;
 use JasterTDC\Warriors\Domain\Weapon\Attribute\AttributeCollection;
+use JasterTDC\Warriors\Domain\Weapon\Exception\InvalidFuseWeaponType;
 use JasterTDC\Warriors\Domain\Weapon\WeaponType\Factory\FromStringWeaponTypeFactory;
 use JasterTDC\Warriors\Domain\Weapon\WeaponType\WeaponType;
 
@@ -34,6 +35,30 @@ final class Weapon
         return $this->weaponName->alias();
     }
 
+    public function fuse(Weapon $weapon): Weapon
+    {
+        if (!$this->equalsWeaponType($weapon->weaponType())) {
+            throw InvalidFuseWeaponType::build($this->weaponTypeValue(), $weapon->weaponTypeValue());
+        }
+
+        $currentWeapon = new self(
+            $this->weaponType(),
+            $this->fullName(),
+            $this->attributeCollection()
+        );
+
+        /** @var Attribute $attribute */
+        foreach ($weapon->attributeCollection() as $attribute) {
+            if ($currentWeapon->hasAttribute($attribute)) {
+                continue ;
+            }
+
+            $currentWeapon->addAttribute($attribute);
+        }
+
+        return $currentWeapon;
+    }
+
     public function addAttribute(Attribute $attribute): void
     {
         $this->attributeCollection->addAttribute($attribute);
@@ -52,6 +77,31 @@ final class Weapon
     public function attributesCount(): int
     {
         return $this->attributeCollection->count();
+    }
+
+    public function attributeCollection(): AttributeCollection
+    {
+        return $this->attributeCollection;
+    }
+
+    private function weaponType(): WeaponType
+    {
+        return $this->weaponType;
+    }
+
+    private function weaponTypeValue(): string
+    {
+        return $this->weaponType->value();
+    }
+
+    private function fullName(): FullName
+    {
+        return $this->weaponName;
+    }
+
+    private function equalsWeaponType(WeaponType $weaponType): bool
+    {
+        return $this->weaponType->equals($weaponType);
     }
 
     public static function buildFromPrimitives(
